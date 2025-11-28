fb531805d999e0c4d86cd689f27b487478c9fef4
ritampaine75-debug <ritampaine75@gmail.com>
2025-11-28 09:45:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,14 @@
 // --- FIREBASE CONFIGURATION ---
 const firebaseConfig = {
-  apiKey: "AIzaSyDUHXK7NYeM6CLNN4fsyGVz1MpIVpJuOq0",
-  authDomain: "studio-vddd5.firebaseapp.com",
-  databaseURL: "https://studio-vddd5-default-rtdb.firebaseio.com",
-  projectId: "studio-vddd5",
-  storageBucket: "studio-vddd5.firebasestorage.app",
-  messagingSenderId: "679230106233",
-  appId: "1:679230106233:web:3d6a91cabc2264e15902a8"
+  apiKey: "AIzaSyCw7as_RexoYbKZHBcGS47bscXfxj_8iTs",
+  authDomain: "shopping-e3ab0.firebaseapp.com",
+  databaseURL: "https://shopping-e3ab0-default-rtdb.firebaseio.com",
+  projectId: "shopping-e3ab0",
+  storageBucket: "shopping-e3ab0.firebasestorage.app",
+  messagingSenderId: "297301068816",
+  appId: "1:297301068816:web:02b29a230f429367abce95"
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
